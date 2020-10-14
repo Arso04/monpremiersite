4ce59b88674927041e097afd5c942019bd1a07e1
Arsen Karahanov <arsenkarahan04@gmail.com>
2020-10-14 05:28:06 -0700

    first commit

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 "# monpremiersite" 
 "# monpremiersite" 
 "# monpremiersite" 
+"# monpremiersite" 
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="style.css">
+    <title></title>
+</head>
+<body>
+<header>
+ <div id= "PPP.jpg" onclick="('#top')">
+            <span>Arsen Karahanov</span>
+        <div id="about">
+            <a href="Accueil"  onclick="slowScroll('#Accueil')"title="Accueil">Accueil</a>
+            <a href="CGU" onclick="slowScroll('#CGU')" title="CGU">CHU</a>
+        </div>
+        <div class="flex-container">
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+</div>
+</header>
+</body>
+</html>
